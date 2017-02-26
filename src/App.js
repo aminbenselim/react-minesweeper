75f87cd45a1ee1fd996567f7cb215bb4344a906b
amine benselim <abens241@gmail.com>
2017-02-27 00:04:43 +0100

    create and render a simple cell grid

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
+import Cell from './cell'
 
 class App extends Component {
   render() {
+    let cells = [];
+    for(let i = 0; i < 100;i++) cells.push(<Cell key={i} />);
     return (
       <div className="App">
+        {cells}
       </div>
     );
   }
---
+++ src/cell.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Cell extends Component {
+    render() {
+        return (
+            <div className='cell'>
+                
+            </div>
+        );
+    }
+}
+
+export default Cell;
