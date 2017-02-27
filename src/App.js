8b86a83563194dd337f0f3a406b01559d3aacf71
amine benselim <abens241@gmail.com>
2017-02-27 21:47:22 +0100

    added rightClick Flagging functionality

--- src/App.js
+++ src/App.js
@@ -1,19 +1,59 @@
 import React, { Component } from "react";
 import "./App.css";
-import Cell from "./cell";
+import Cell from "./Cell";
 //import game logic
 import { getRandomBombCells, getNumOfNeighbouringBombs } from "./game";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      flags: 20,
+      clicked: []
+    };
+    this.bombCells = getRandomBombCells();
+    this.nbs = getNumOfNeighbouringBombs(this.bombCells);
+    //this.onLeftClick = this.onLeftClick().bind(this);
+  }
+  onRightClick(i, e) {
+    e.preventDefault();
+    let clicked = this.state.clicked;
+    if (clicked.indexOf(i) === -1) {
+      if (this.state.flags > 0) {
+        clicked = clicked.concat(i);
+        let newFlags = this.state.flags - 1;
+        this.setState({
+          flags: newFlags,
+          clicked
+        });
+      }
+    } else {
+      clicked.splice(clicked.indexOf(i), 1);
+      let newFlags = this.state.flags + 1;
+      this.setState({
+        flags: newFlags,
+        clicked
+      });
+    }
+  }
   render() {
-    const bombCells = getRandomBombCells();
-    const nbs = getNumOfNeighbouringBombs(bombCells);
     let cells = [];
     for (let i = 0; i < 100; i++)
-      cells.push(<Cell key={i}>{nbs[i]}</Cell>);
+      cells.push(
+        <Cell
+          key={i}
+          right={this.onRightClick.bind(this, i)}
+        >
+        {(this.state.clicked.indexOf(i) !== -1 ) ? 'F' : this.nbs[i]}
+        </Cell>
+  
+      );
     return (
-      <div className="App">
-        {cells}
+      <div className="container">
+        <span> {this.state.flags} </span>
+        <div className="App">
+          {cells}
+        </div>
       </div>
     );
   }
--- src/cell.js
+++ src/cell.js
@@ -1,13 +1,16 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Cell extends Component {
-    render() {
-        return (
-            <div className='cell'>
-                {this.props.children}
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div
+        className="cell"
+        onContextMenu={this.props.right}
+        onClick={this.props.left}
+      >
+      {this.props.children}
+      </div>
+    );
+  }
 }
-
 export default Cell;
